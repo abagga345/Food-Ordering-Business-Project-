d1e34409c5a15a90c1d89e51d6c49a4593c8b99c
AAYUSH BAGGA <aayushbagga2005@gmail.com>
2024-10-01 22:39:49 +0530

    Updated view particular menu item route

--- food_ordering/app/api/viewMenuItem/route.ts
+++ food_ordering/app/api/viewMenuItem/route.ts
@@ -9,26 +9,27 @@ export async function GET(req:NextRequest){
     try{
         let availableItem=await prisma.menu.findFirst({
             where:{
-                visibility:true,
-                available:true,
                 id:itemId
             },
             select:{
                 id:true,
                 amount:true,
                 title:true,
-                imageUrl:true
+                imageUrl:true,
+                visibility:true,
+                available:true,
             }
         });
         if (availableItem===null){
-            return NextResponse.json({"message":"Item not available"},{status:400});
+            return NextResponse.json({"message":"Item not found"},{status:400});
         }
         return NextResponse.json({
             "message":"Menu Item Fetched Successully",
             id:availableItem["id"],
             title:availableItem["title"],
             imageUrl:availableItem["imageUrl"],
-            amount:availableItem["amount"]
+            amount:availableItem["amount"],
+            visibility:availableItem["visibility"]&&availableItem["available"]
         });
     }catch(err){
         return NextResponse.json({"message":"Internal Server Error"},{status:500});
